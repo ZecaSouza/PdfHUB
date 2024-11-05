d32d65053d2832e4af347111bb0563de36668e89
Zeca Souza <szeca00@gmail.com>
2024-11-04 21:15:23 -0300

    Initial commit

---
+++ src/test/kotlin/com/APIRestKotlin/APIRestKotlin/ApiRestKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.APIRestKotlin.APIRestKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiRestKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
